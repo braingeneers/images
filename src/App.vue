94e3de6bf14d8c766ff470321cc0a4dbecccfa2e
Pierre Baudin <pvbaudin@ucsc.edu>
2021-04-12 17:08:56 -0700

    added load beginning functionality, need to set beginning image viewer to start still

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,21 @@
     <label>group identifier</label>
     <input v-model="groupID" placeholder="C">
     <button v-on:click="$refs.images.load()">Load</button>
+    <button v-on:click="$refs.images2.load()">Load Begining</button>
     <Images ref="images" v-bind:groupID="groupID" v-bind:uuid="uuid" v-bind:endpoint="endpoint"/>
+    <Images2 ref="images2" v-bind:groupID="groupID" v-bind:uuid="uuid" v-bind:endpoint="endpoint"/>
   </div>
 </template>
 
 <script>
 import Images from "./components/Images.vue"
+import Images2 from "./components/Images2.vue"
 
 export default {
   name: "app",
   components: {
-    Images
+    Images,
+    Images2
   },
   data: function () {
     return {
